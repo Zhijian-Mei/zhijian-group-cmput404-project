1edf96d8b2d528c31ae24660ec5280516c956e8c
jieyuan-bi <55514311+jieyuan-bi@users.noreply.github.com>
2022-02-26 13:32:55 -0700

    changed author serializer

--- mysite/socialdistribution/serializers.py
+++ mysite/socialdistribution/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuthorModel
-        fields = ['id', 'host', 'displayName', 'url', 'github', 'profileImage']
+        fields = ['id', 'host', 'displayName', 'github', 'profileImage']
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(read_only=True)
