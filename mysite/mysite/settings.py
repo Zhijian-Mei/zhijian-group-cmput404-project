843c5de262ec37cf45b752088785d5963d2187fc
YijieGu <73412566+YijieGu@users.noreply.github.com>
2022-03-04 00:56:29 -0700

    still some mistakes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'socialdistribution.apps.SocialdistributionConfig',
 ]
 
@@ -131,4 +130,3 @@ HOST_URL = 'http://127.0.0.1:8000/'
 
 LOGIN_REDIRECT_URL = "/"
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
